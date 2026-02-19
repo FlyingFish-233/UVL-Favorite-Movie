2b117162a43a51938d729fb46979ba346bb68867
FlyingFish-233 <loftythinking@126.com>
2026-02-19 19:54:56 +0800

    update base url for BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/UVL-Favorite-Movie/">
             <App />
         </BrowserRouter>
     </StrictMode>,
